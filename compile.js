b80499c6940e03405c5500963888e42f8f68dbbc
rudchyk <sergii.rudchyk@gmail.com>
2022-01-04 18:41:23 +0200

    added compile.js and inbox.sol

--- compile.js
+++ compile.js
@@ -1,1 +1,9 @@
-// compile code will go here
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+const compiled = solc.compile(source, 1);
+
+module.exports = compiled.contracts[':Inbox'];
